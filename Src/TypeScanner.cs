c39eee584c31de91470bd099a72d2db9bb666f53
Mike Minutillo <michael.minutillo@gmail.com>
2011-05-07 10:29:24 +0800

    Clean up type scanner and remove lazy initialization of assembly types

--- Src/TypeScanner.cs
+++ Src/TypeScanner.cs
@@ -20,24 +20,13 @@ namespace Conventional
             _conventions.Add(convention);
         }
 
-        internal void Run(Func<Type, Action<Type>> installers)
+        internal IEnumerable<Registration> GetRegistrations()
         {
             var types = _typeSource.GetTypes().ToList();
 
-            foreach (var convention in _conventions)
-                foreach (var type in types)
-                    if (convention.Matches(type))
-                        installers(convention.GetType())(type);
-        }
-
-        internal IEnumerable<Registration> GetRegistrations()
-        {
-            var types = new Lazy<IEnumerable<Type>>(() => _typeSource.GetTypes().ToList(),
-                                                     LazyThreadSafetyMode.ExecutionAndPublication);
-
             return from c in _conventions.AsParallel().AsOrdered()
                    let ct = c.GetType()
-                   from t in types.Value
+                   from t in types
                    where c.Matches(t)
                    select new Registration(ct, t);
         }
